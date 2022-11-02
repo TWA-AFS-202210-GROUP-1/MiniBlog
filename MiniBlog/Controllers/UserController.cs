aa758108338887443c63dcb21eb857eacd6d4d71
Minghao QU <MQu2@slb.com>
2022-11-02 16:07:20 +0800

    fix: replace UserStoreWillReplaceFuture with injected articleStore

--- MiniBlog/Controllers/UserController.cs
+++ MiniBlog/Controllers/UserController.cs
@@ -4,61 +4,68 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MiniBlog.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class UserController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class UserController : ControllerBase
+  {
+    private IArticleStore articleStore;
+
+    public UserController(IArticleStore articleStore)
     {
-        [HttpPost]
-        public IActionResult Register(User user)
-        {
-            if (!UserStoreWillReplaceInFuture.Instance.GetAll().Exists(_ => user.Name.ToLower() == _.Name.ToLower()))
-            {
-                UserStoreWillReplaceInFuture.Instance.Save(user);
-            }
-
-            return Created("/user", user);
-        }
-
-        [HttpGet]
-        public List<User> GetAll()
-        {
-            return UserStoreWillReplaceInFuture.Instance.GetAll();
-        }
-
-        [HttpPut]
-        public User Update(User user)
-        {
-            var foundUser = UserStoreWillReplaceInFuture.Instance.GetAll().FirstOrDefault(_ => _.Name == user.Name);
-            if (foundUser != null)
-            {
-                foundUser.Email = user.Email;
-            }
-
-            return foundUser;
-        }
-
-        [HttpDelete]
-        public User Delete(string name)
-        {
-            var foundUser = UserStoreWillReplaceInFuture.Instance.GetAll().FirstOrDefault(_ => _.Name == name);
-            if (foundUser != null)
-            {
-                UserStoreWillReplaceInFuture.Instance.Delete(foundUser);
-                var articles = ArticleStoreWillReplaceInFuture.Instance.GetAll()
-                    .Where(article => article.UserName == foundUser.Name)
-                    .ToList();
-                articles.ForEach(article => ArticleStoreWillReplaceInFuture.Instance.Delete(article));
-            }
-
-            return foundUser;
-        }
-
-        [HttpGet("{name}")]
-        public User GetByName(string name)
-        {
-            return UserStoreWillReplaceInFuture.Instance.GetAll().FirstOrDefault(_ =>
-                string.Equals(_.Name, name, StringComparison.CurrentCultureIgnoreCase)) ?? throw new
-                InvalidOperationException();
-        }
+      this.articleStore = articleStore;
     }
+
+    [HttpPost]
+    public IActionResult Register(User user)
+    {
+      if (!UserStoreWillReplaceInFuture.Instance.GetAll().Exists(_ => user.Name.ToLower() == _.Name.ToLower()))
+      {
+        UserStoreWillReplaceInFuture.Instance.Save(user);
+      }
+
+      return Created("/user", user);
+    }
+
+    [HttpGet]
+    public List<User> GetAll()
+    {
+      return UserStoreWillReplaceInFuture.Instance.GetAll();
+    }
+
+    [HttpPut]
+    public User Update(User user)
+    {
+      var foundUser = UserStoreWillReplaceInFuture.Instance.GetAll().FirstOrDefault(_ => _.Name == user.Name);
+      if (foundUser != null)
+      {
+        foundUser.Email = user.Email;
+      }
+
+      return foundUser;
+    }
+
+    [HttpDelete]
+    public User Delete(string name)
+    {
+      var foundUser = UserStoreWillReplaceInFuture.Instance.GetAll().FirstOrDefault(_ => _.Name == name);
+      if (foundUser != null)
+      {
+        UserStoreWillReplaceInFuture.Instance.Delete(foundUser);
+        var articles = articleStore.GetAll()
+            .Where(article => article.UserName == foundUser.Name)
+            .ToList();
+        articles.ForEach(article => articleStore.Delete(article));
+      }
+
+      return foundUser;
+    }
+
+    [HttpGet("{name}")]
+    public User GetByName(string name)
+    {
+      return UserStoreWillReplaceInFuture.Instance.GetAll().FirstOrDefault(_ =>
+          string.Equals(_.Name, name, StringComparison.CurrentCultureIgnoreCase)) ?? throw new
+          InvalidOperationException();
+    }
+  }
 }
