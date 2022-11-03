cd4eff177b6357073e9388af74c505154888b23b
xma12 <xma12@slb.com>
2022-11-03 11:00:27 +0800

    feat: change program config

--- MiniBlog/Program.cs
+++ MiniBlog/Program.cs
@@ -9,8 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IUserService, UserService>();
-builder.Services.AddSingleton<IArticleService, ArticleService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IArticleService, ArticleService>();
 builder.Services.AddSingleton<IUserStore, UserStoreContext>();
 builder.Services.AddSingleton<IArticleStore, ArticleStoreContext>();
 
